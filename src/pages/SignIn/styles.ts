b700c6b51011e10a1332ea9dfc63c871da32f2aa
ivanvinicius <ivanvinicius88@gmail.com>
2020-10-21 09:57:49 -0300

    pagina de login

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,107 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import signInBackground from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      width: 100%;
+
+      background-color: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      color: #f4ede8;
+
+      &::placeholder {
+        color: #666360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      width: 100%;
+      height: 56px;
+      margin-top: 16px;
+
+      border: 0;
+      background-color: #ff9000;
+      color: #312e38;
+      font-weight: 500;
+      border-radius: 10px;
+      padding: 0 16px;
+
+      transition: background 0.2s;
+
+      &:hover {
+        background-color: ${shade(0.2, '#ff9000')};
+      }
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    display: flex;
+    align-items: center;
+
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackground}) no-repeat center;
+  background-size: cover;
+`;
