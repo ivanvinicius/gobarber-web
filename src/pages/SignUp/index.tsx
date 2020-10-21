232aeedf5e520454d0bbc4ff516f688342423e62
ivanvinicius <ivanvinicius88@gmail.com>
2020-10-21 14:04:54 -0300

    pagina de cadastro

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FiUser, FiArrowLeft, FiMail, FiLock } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="logo" />
+
+        <form>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button>Cadastrar</Button>
+        </form>
+
+        <a href="/signin">
+          <FiArrowLeft />
+          Voltar ao logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
