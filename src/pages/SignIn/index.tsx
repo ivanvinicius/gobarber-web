680b22db40a51df51625b56cf5c17b451040c012
ivanvinicius <ivanvinicius88@gmail.com>
2020-10-21 16:23:10 -0300

    validação de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
-import logoImg from '../../assets/logo.svg';
-
+import getValidationErrors from '../../utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
+interface ISignInFormaData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(
+    async ({ email, password }: ISignInFormaData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .email('E-mail precisa ser válido')
+            .required('E-mail obrigatório'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        await schema.validate({ email, password }, { abortEarly: false });
+      } catch (err) {
+        const formattedErrors = getValidationErrors(err);
+
+        formRef.current?.setErrors(formattedErrors);
+      }
+    },
+    [],
+  );
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="logo" />
 
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
 
           <Input name="email" icon={FiMail} placeholder="E-mail" />
@@ -25,10 +57,10 @@ const SignIn: React.FC = () => {
             placeholder="Senha"
           />
 
-          <Button>Entrar</Button>
+          <Button type="submit">Entrar</Button>
 
           <a href="/forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
 
         <a href="/signup">
           <FiLogIn />
